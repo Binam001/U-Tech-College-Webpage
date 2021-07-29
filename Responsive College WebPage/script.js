0ca458d8e55f550552219e06d4f5aeb22e76f956
Binam001 <87900326+Binam001@users.noreply.github.com>
2021-07-29 13:28:11 +0545

    Add files via upload

---
+++ Responsive College WebPage/script.js
@@ -0,0 +1,14 @@
+function applynow(){
+    document.getElementById("adm-form").style.display="block";
+}
+
+function x(){
+    document.getElementById("adm-form").style.display="none";
+}
+
+function bar(){
+    document.getElementById("menu").style.display="block";
+}
+function menuclose(){
+    document.getElementById("menu").style.display="none";
+}
